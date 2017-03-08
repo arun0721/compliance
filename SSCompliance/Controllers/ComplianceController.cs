bcbeea7b6a43f7821df4cb4a0fa96a5d2eafd552
arun0721 <arun0721@yahoo.com>
2017-03-07 22:07:03 -0600

    Add files via upload

---
+++ SSCompliance/Controllers/ComplianceController.cs
@@ -0,0 +1,59 @@
+﻿using RestSharp;
+using SSCompliance.Models;
+using SSCompliance.ServiceApi;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+
+namespace SSCompliance.Controllers
+{
+    public class ComplianceController : ApiController
+    {
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetResponse()
+        {
+            NameValueCollection ncv = HttpUtility.ParseQueryString(Request.RequestUri.Query);
+            TradeGovService tds = new TradeGovService();
+            var _response = tds.GetTradeGovResponse(ncv);
+
+            return Ok(_response);
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.ActionName("FindAll")]
+        public IHttpActionResult AllResponses([FromBody]SearchRequest searchRequest)
+        {
+            if (searchRequest == null)
+            {
+                return BadRequest("Invalid passed data");
+            }
+
+            if (string.IsNullOrEmpty(searchRequest.api_key))
+            {
+                return BadRequest("Please pass api key");
+            }
+
+            if (searchRequest.queries == null || searchRequest.queries.Count == 0)
+            {
+                return BadRequest("Invalid passed data");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            TradeGovService tds = new TradeGovService();
+            var responses = tds.GetTradeGovResponses(searchRequest);
+            return Ok(responses);
+        }
+    }
+}
---
+++ SSCompliance/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using SSCompliance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSCompliance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            SSComplianceLogin user = new SSComplianceLogin();
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Index(SSComplianceLogin user)
+        {
+            bool success = false;
+            var display = UserLogins().Where(u => u.UserName == user.UserName && u.UserPassword == user.UserPassword).FirstOrDefault();
+            if(display!=null)
+            {
+                ViewBag.Status = "Correct UserName and Password";
+                success = true;
+            }
+            else
+            {
+                ViewBag.Status = "Incorrect UserName or Password";
+            }
+
+            if (success)
+                return RedirectToAction("ComplianceSearch", "Find");
+
+            return View(user);
+        }
+
+        public List<SSComplianceLogin> UserLogins()
+        {
+            List<SSComplianceLogin> users = new List<SSComplianceLogin>();
+            users.Add(new SSComplianceLogin { UserName = "Jon", UserPassword = "password" });
+            return users;
+        }
+    }
+}
